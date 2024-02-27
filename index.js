00ae60cda2e6e61f171cd876e0207c653b44726e
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-02-27 17:35:00 +0000

    added question objects within questions array and worked on program flow

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "Please enter title: ",
+    default: "My Repository",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description: ",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please enter installation instructions: ",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter usage information: ",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please enter contribution guidelines: ",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please enter test instructions: ",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "Please enter your github username: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email for contact: ",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  console.log("writing to file...");
+  let text = generateMarkdown(data);
+  console.log(text);
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    writeToFile("README.md", answers);
+  });
 }
 
 // function call to initialize program

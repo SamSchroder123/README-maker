e3ffb7e7a915fffc0f9ce2c769c66e89bdfc7e31
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-03-02 14:47:38 +0000

    badge functionality fixedc

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,13 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   let badge = "";
-  if ((data.license = "Creative Commons")) {
+  if (data.license === "Creative Commons") {
     badge =
       "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
-  } else if ((data.license = "MIT")) {
+  } else if (data.license === "MIT") {
     badge =
       "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  } else if ((data.license = "Mozilla")) {
+  } else if (data.license === "Mozilla") {
     badge =
       "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
   }

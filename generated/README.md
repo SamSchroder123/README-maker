dd2a63fdb941555c969b4dfbdcaeac6272029def
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-03-02 14:32:35 +0000

    added badge functionality (broken)

--- generated/README.md
+++ generated/README.md
@@ -1,33 +1,35 @@
-# My Repository
-## Table of Contents
-[Description](#description)
+[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+  # README-Generator
+  ## Table of Contents
+  [Description](#description)
 
-[Installation](#installation)
+  [Installation](#installation)
 
-[Usage](#usage)
+  [Usage](#usage)
 
-[Licence](#license)
+  [Licence](#license)
 
-[Contributing](#contributing)
+  [Contributing](#contributing)
 
-[Tests](#tests)
+  [Tests](#tests)
 
-[Questions](#questions)
-## Description
+  [Questions](#questions)
+  ## Description
+  generates a README from the command line
+  ## Installation
+  pull from github then run node index.js and enter values for README
+  ## Usage
+  run node index.js
+  ## Licence
+  Creative Commons
+  ## Contributing
+  freely contribute
+  ## Tests
+  
+  ## Questions
+  GitHub username: [SamSchroder123](https://github.com/SamSchroder123)
 
-## Installation
+  Contact Email: samueltillotsonschroder@gmail.com
 
-## Usage
-
-## Licence
-MIT
-## Contributing
-
-## Tests
-
-## Questions
-GitHub username: [SamSchroder123](https://github.com/SamSchroder123)
-
-Contact Email: 
-
-Please email me for further questions.
+  Please email me for further questions.
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,39 +1,51 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-## Table of Contents
-[Description](#description)
+  let badge = "";
+  if ((data.license = "Creative Commons")) {
+    badge =
+      "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
+  } else if ((data.license = "MIT")) {
+    badge =
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if ((data.license = "Mozilla")) {
+    badge =
+      "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  }
+  return `${badge}
+  # ${data.title}
+  ## Table of Contents
+  [Description](#description)
 
-[Installation](#installation)
+  [Installation](#installation)
 
-[Usage](#usage)
+  [Usage](#usage)
 
-[Licence](#license)
+  [Licence](#license)
 
-[Contributing](#contributing)
+  [Contributing](#contributing)
 
-[Tests](#tests)
+  [Tests](#tests)
 
-[Questions](#questions)
-## Description
-${data.description}
-## Installation
-${data.installation}
-## Usage
-${data.usage}
-## Licence
-${data.license}
-## Contributing
-${data.contributing}
-## Tests
-${data.tests}
-## Questions
-GitHub username: [${data.username}](https://github.com/${data.username})
+  [Questions](#questions)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Licence
+  ${data.license}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  GitHub username: [${data.username}](https://github.com/${data.username})
 
-Contact Email: ${data.email}
+  Contact Email: ${data.email}
 
-Please email me for further questions.
-`;
+  Please email me for further questions.
+  `;
 }
 
 module.exports = generateMarkdown;
